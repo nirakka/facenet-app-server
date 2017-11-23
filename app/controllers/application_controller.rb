6f043099af54321f1ef9263c2a094e10c68e4ee9
nirakka <ikoa.syo@gmail.com>
2017-11-23 20:13:16 +0900

    complet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,57 @@
+require 'net/http'
+require 'uri'
+require 'mechanize'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def home
-    @rankings = Ranking.all
+    @rankings = Ranking.order("score asc")
     render "layouts/test"
   end
+
+  def about
+    render "ranking/about"
+  end
+
+  def show
+    render "ranking/show"
+  end
+
+  def upload
+    image1 = params[:image1]
+    image1_path = Rails.root.join('public', image1.original_filename)
+    File.open(image1_path, 'wb') do |file|
+      file.write(image1.read)
+    end
+    image2 = params[:image2]
+    image2_path = Rails.root.join('public', image2.original_filename)
+    File.open(image2_path, 'wb') do |file|
+      file.write(image2.read)
+    end
+    puts "success"
+    score = apipost(File.open("/#{image1_path}"), File.open("/#{image2_path}"))
+
+    Ranking.create([person1: params[:person1],
+                    person1_path: image1.original_filename,
+                    person2: params[:person2],
+                    person2_path: image2.original_filename,
+                    score: score])
+    
+    redirect_to('/') unless score.nil?
+    
+  end
+
+  def apipost(file1, file2)
+    request_url = "https://www.floydlabs.com/expose/FKB3MpcMLYYitHkQT79EFD"
+    agent = Mechanize.new
+    response = agent.post(request_url, {
+                            file1:  file1,
+                            file2: file2
+                          })
+    return response.body
+   
+
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ranking.create([person1: "小籔千豊", person2: "ぺ・チョンソク", person1_path: "koyabu.jpeg", person2_path:"pechonzoku.jpeg", score: 0.877884030342102])
+
+Ranking.create([person1: "近藤春菜", person2: "すだ和", person1_path: "haruna.jpeg", person2_path:"suda.jpeg", score: 0.9229482412338257])
+
